1fe95dcc7918e95c8f9db6ed93f408e3af3b773d
German Mauter <mauter.german@yandex.ru>
2022-02-18 03:22:54 +0500

    Добавил кнопку в Кэрроте/Jquery

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 // Создание функций на странице браузера
 
+function searchUserId() {
+  return new Promise ((resolve) => {    
+    if (window.location.origin == "https://carrotquest.io") {
+      function searchUserIdScript() {
+        if ($('[ng-bind^="vm.user.user_id"]').text().length>15 && $(".go_marquiz").length<1) {
+          let userId = document.querySelector('[ng-bind^="vm.user.user_id"]').textContent
+          let email = document.querySelector(".email");
+  
+          let button = document.createElement('a');
+          button.innerHTML = 'Админка';  
+          button.setAttribute("href", "https://panel.marquiz.ru/admin/user/"+userId);
+          button.setAttribute("target", "_blank");
+          button.setAttribute("class", "go_marquiz");
+          button.setAttribute("style", "background: #d34085;color: #fff;border-radius: 4px;padding: 4px 11px;");   
+          email.insertAdjacentElement('afterEnd', button);
+        }      
+      }
+      searchUserIdScript();
+      $("cq-conversation-list-item").click(function() { 
+        setTimeout(searchUserIdScript, 500);
+      });
+    }     
+  })
+  // функция добавляет кнопку Админка в кэрроте
+}
+
+
+
 function searchQuizId () {
   return new Promise ((resolve) => {
-    
+   
     let link = document.querySelector('.whitelabel__container a')
     let array, id;
     if (link) {
@@ -28,6 +56,7 @@ async function getHttp(quizId) {
 
 function startSearch () {
   // Функция объединяющая все действия с данными
+  searchUserId();
   searchQuizId ()
     .then((quizId) => getHttp(quizId))
     .then((data) => { 
